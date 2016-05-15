f455a20a66deaeeb6b497a8a6ccafaa4cc411c3c
Bufigol <felipevasquez.tort@gmail.com>
2016-05-15 20:21:34 +0200

    Primera version del tratamiento de json.
    Se ha empezado a desarrollar el controlador que va a tratar los archivos json para que devuelvan los objetos de las clases correspondientes.

--- app/src/main/java/com/labs/josemanuel/reportcenter/Model/Propuesta.java
+++ app/src/main/java/com/labs/josemanuel/reportcenter/Model/Propuesta.java
@@ -1,5 +1,7 @@
 package com.labs.josemanuel.reportcenter.Model;
 
+import java.util.Arrays;
+
 /**
  * Created by bufigol on 14/05/16.
  */
@@ -18,4 +20,142 @@ public class Propuesta {
     private Imagen[] image;
     private Type type;
 
+    public Propuesta(String title,
+                     String langcode,
+                     int nid, String uuid,
+                     long created,
+                     long changed,
+                     Body body,
+                     Usuario uid,
+                     Comentario[] com,
+                     Localizacion loc,
+                     Imagen[] image,
+                     Type type) {
+        this.title = title;
+        this.langcode = langcode;
+        this.nid = nid;
+        this.uuid = uuid;
+        this.created = created;
+        this.changed = changed;
+        this.body = body;
+        this.uid = uid;
+        this.com = com;
+        this.loc = loc;
+        this.image = image;
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLangcode() {
+        return langcode;
+    }
+
+    public void setLangcode(String langcode) {
+        this.langcode = langcode;
+    }
+
+    public int getNid() {
+        return nid;
+    }
+
+    public void setNid(int nid) {
+        this.nid = nid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public long getCreated() {
+        return created;
+    }
+
+    public void setCreated(long created) {
+        this.created = created;
+    }
+
+    public long getChanged() {
+        return changed;
+    }
+
+    public void setChanged(long changed) {
+        this.changed = changed;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public Usuario getUid() {
+        return uid;
+    }
+
+    public void setUid(Usuario uid) {
+        this.uid = uid;
+    }
+
+    public Comentario[] getCom() {
+        return com;
+    }
+
+    public void setCom(Comentario[] com) {
+        this.com = com;
+    }
+
+    public Localizacion getLoc() {
+        return loc;
+    }
+
+    public void setLoc(Localizacion loc) {
+        this.loc = loc;
+    }
+
+    public Imagen[] getImage() {
+        return image;
+    }
+
+    public void setImage(Imagen[] image) {
+        this.image = image;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Propuesta{" +
+                "title='" + title + '\'' +
+                ", langcode='" + langcode + '\'' +
+                ", nid=" + nid +
+                ", uuid='" + uuid + '\'' +
+                ", created=" + created +
+                ", changed=" + changed +
+                ", body=" + body +
+                ", uid=" + uid +
+                ", com=" + Arrays.toString(com) +
+                ", loc=" + loc +
+                ", image=" + Arrays.toString(image) +
+                ", type=" + type +
+                '}';
+    }
 }
