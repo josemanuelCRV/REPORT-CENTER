a51c8c33203603a19fe0fe48dd6503e7a8046fd9
Bufigol <felipevasquez.tort@gmail.com>
2016-05-16 13:39:15 +0200

    Avances en el tratamiento de json.
    Ya estan desarrollada la primera version de la clase. Aun faltan la correcion y mejora de los mismos.

--- app/src/main/java/com/labs/josemanuel/reportcenter/Controler/jsoncontroler.java
+++ app/src/main/java/com/labs/josemanuel/reportcenter/Controler/jsoncontroler.java
@@ -12,31 +12,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
  * Created by bufigol on 15/05/16.
  */
 public class jsoncontroler {
-    /*
-            try {
-            Comentario[] output;
-            JSONArray array = (JSONArray) jsoninput.get("field_proposal_comments");
-            output = new Comentario[array.length()];
-            for(int i = 0;i < output.length; i++){
-                output[i] = getSingleComment(array.getJSONObject(i));
-            }
-            return output;
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return null;
-        }
-     */
-    public static Propuesta[] generateProupestaArray(JSONArray jsoninput){
+
+    public static ArrayList<Propuesta> generateProupestaArray(JSONArray jsoninput){
         try {
-        Propuesta[] output = new Propuesta[jsoninput.length()];
-        for (int i=0;i<output.length;i++) {
-            output[i] = generatePropuesta(jsoninput.getJSONObject(i));
+            ArrayList<Propuesta> output = new ArrayList<Propuesta>();
+        for (int i=0;i<jsoninput.length();i++) {
+            output.add(generatePropuesta(jsoninput.getJSONObject(i)));
         }
             return output;
         }
@@ -137,16 +125,6 @@ public class jsoncontroler {
         Usuario uid          = null;
         int     count         = getIntFromNode(jsonObject,"comment_count");
         return  new Comentario(status,id,timestamp,name,uid,count);
- /*
-     public Comentario(int status, int id, Date timestamp, String name, Usuario uid, int count) {
-        this.status = status;
-        this.id = id;
-        this.timestamp = timestamp;
-        this.name = name;
-        this.uid = uid;
-        this.count = count;
-    }
-*/
     }
 
     private static Date getDateFromNode(JSONObject object) {
@@ -162,15 +140,62 @@ public class jsoncontroler {
     }
 
     private static Type getTypeFromNode(JSONObject jsoninput) {
-        return null;
-
+        try {
+            JSONArray array = (JSONArray) jsoninput.getJSONArray("type");
+            JSONObject data = array.getJSONObject(0);
+            String target_id   = getStringFromNode(data, "target_id");
+            String target_type = getStringFromNode(data, "target_type");
+            String target_uuid = getStringFromNode(data, "target_uuid");
+            return new Type(target_id,target_type,target_uuid);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private static Imagen[] getImagenArray(JSONObject jsoninput) {
-        return null;
+        try {
+            JSONArray array = (JSONArray) jsoninput.getJSONArray("field_proposal_images");
+            Imagen[] ouput = new Imagen[array.length()];
+            for (int i = 0;i<array.length();i++){
+                JSONObject data = array.getJSONObject(i);
+                int    target_id   = getIntFromNode(data, "target_id");
+                String alt         = getStringFromNode(data, "alt") ;
+                String title       = getStringFromNode(data,"title");
+                int    width       = getIntFromNode(data, "width");
+                int    height      = getIntFromNode(data, "height");
+                String target_type = getStringFromNode(data, "target_type");
+                String target_uuid = getStringFromNode(data, "target_uuid");
+                String url         = getStringFromNode(data, "url");
+                ouput[i] = new Imagen(target_id,alt,title,width,height,target_type,target_uuid,url);
+            }
+            return ouput;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private static Localizacion getLocalizacionFromNode(JSONObject jsoninput) {
-        return null;
+        try {
+            JSONArray array = (JSONArray) jsoninput.getJSONArray("field_proposal_location");
+            JSONObject data = array.getJSONObject(0);
+            double latitude = getDoubleFromNode(data, "lat");
+            double longitude = getDoubleFromNode(data,"lng");
+            return new Localizacion(latitude,longitude);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static double getDoubleFromNode(JSONObject data, String nodeName) {
+        try {
+            return  data.getDouble(nodeName);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return Double.parseDouble(null);
+        }
+
     }
 }
