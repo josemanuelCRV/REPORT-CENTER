80c00ebb0d1c7b45d027fff22a0aa4cc712f29d4
josemanuelCRV <josemanuel.crv@gmail.com>
2016-05-27 03:40:04 +0200

    Fase de desarrollo: v.1.2
    Avances en :
    · app_bar - toolbar
    · NavigationDrawer
    · icons
    · colors

--- app/src/main/java/com/labs/josemanuel/reportcenter/ui/actividades/MainActivity.java
+++ app/src/main/java/com/labs/josemanuel/reportcenter/ui/actividades/MainActivity.java
@@ -33,24 +33,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             CHECK TOOLBAR
          */
         // Creación del Toolbar y el NavigationDrawer
-        //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-         //emptyFeedTextView= (TextView) findViewById(R.id.empty_view);
-        // setSupportActionBar(toolbar);
+        //emptyFeedTextView= (TextView) findViewById(R.id.empty_view);
+        setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        /*ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);*/
 
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         drawer.setDrawerListener(toggle);
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
 
 
         // Creación del fragmento principal
