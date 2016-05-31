bade872f579933a88b9d22b2a0ce4d008f37713f
Miguel <mhalys91@gmail.com>
2016-05-31 10:53:34 +0200

    Revert "Merge Prueba-Interfaces + Logica-JSON"
    This reverts commit bf4d38c8c0cd72acf3246a28164bae58a5a9dbb5.

--- app/src/main/java/com/labs/josemanuel/reportcenter/ui/actividades/MainActivity.java
+++ app/src/main/java/com/labs/josemanuel/reportcenter/ui/actividades/MainActivity.java
@@ -88,9 +88,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
        /* ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        */
+*/
 
-        //Dummy
         drawer.setDrawerListener(toggle);
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
